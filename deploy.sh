8b03ef11adc97417e91f063b3c810b4fa989d35f
lucasst626 <lucasst626@gmail.com>
2025-02-06 12:10:16 +0100

    asddf

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+apt update
+
+apt install npm nodejs certbot python3-certbot-nginx
+
+npm install
+
+certbot certanly --standalone -d dev7.cyberbunny.online --non-interactive --agree-tos -m lucasst626@gmail.com
+
+cp /etc/letsencrypt/live/dev7.cyberbunny.online/fullchain.pem /etc/ssl/certs/dev7.cyberbunny.online.crt
+
+cp /etc/letsencrypt/live/dev7.cyberbunny.online/privkey.pem /etc/ssl/private/dev7.cyberbunny.online.key
