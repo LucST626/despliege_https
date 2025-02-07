e0106e35f125dcbdcea2690f9b4613c245c4b4e7
lucasst626 <lucasst626@gmail.com>
2025-02-07 09:56:44 +0100

    sa

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ apt install npm nodejs certbot python3-certbot-nginx
 
 npm install
 
-certbot certanly --standalone -d dev7.cyberbunny.online --non-interactive --agree-tos -m lucasst626@gmail.com
+certbot certanly --standalone -d dev5.cyberbunny.online --non-interactive --agree-tos -m lucasst626@gmail.com
 
-cp /etc/letsencrypt/live/dev7.cyberbunny.online/fullchain.pem /etc/ssl/certs/dev7.cyberbunny.online.crt
+cp /etc/letsencrypt/live/dev5.cyberbunny.online/fullchain.pem /etc/ssl/certs/dev7.cyberbunny.online.crt
 
-cp /etc/letsencrypt/live/dev7.cyberbunny.online/privkey.pem /etc/ssl/private/dev7.cyberbunny.online.key
+cp /etc/letsencrypt/live/dev5.cyberbunny.online/privkey.pem /etc/ssl/private/dev7.cyberbunny.online.key
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
+const http = require('http');
 const app = express();
 app.use(express.json());
 const {getMessages, addMessage} = require('./database.js');
@@ -40,17 +41,25 @@ app.post('/message', (req, res) => {
   }
 })
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, 'privkey.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'fullchain.pem'))
-};
-
-
-//create an http server
-https.createServer(options, app).listen(3000, () => {
-  console.log('Servidor corriendo en https://dev7.cyberbunny.online:3000');
+if(process.env.NODE_ENV === 'production'){
+ 
+  const options = {
+    key: fs.readFileSync(path.join(__dirname, 'privkey.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'fullchain.pem'))
+  };
+  
+  
+  //create an http server
+  https.createServer(options, app).listen(3000, () => {
+    console.log('Servidor corriendo en https://dev5.cyberbunny.online:3000');
+  })
+} else {
+  http.createServer(app).listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+  })
 }
-);
+
+;
 /*
 app.listen(3000, () => {    
     console.log('Servidor corriendo en http://localhost:3000');
