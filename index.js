afe545ea5c4d8f9e7b716cab5ba80d367f767449
lucasst626 <lucasst626@gmail.com>
2025-02-13 12:40:57 +0100

    wqe

--- index.js
+++ index.js
@@ -1,58 +1,83 @@
+// require('dotenv').config(); // Cargar variables de entorno, se puede settear desde terminal 'export NODE_ENV="dev"'
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
 const http = require('http');
-const cors = require('cors'); // Requiere la librería cors
+const path = require('path');
+const cors = require('cors');
+const {getMessages, addMessage} = require('./database.js');
+
+const port = 3000;
 const app = express();
 app.use(express.json());
-app.use(cors()); // Usa cors en tu aplicación
-const { getMessages, addMessage } = require('./database.js');
-const path = require('path');
+app.use(express.static('public'));
 
 const APIKEY = "123456";
 
+app.use(cors()); // Se puede añadir a todas las rutas
+
+// CORS middleware
+/*
+const corsOptions = {
+  origin: ['https://dev1.cyberbunny.online:3000', 'https://alma17.neocities.org/practica_vps'],
+  optionsSuccessStatus: 200,
+};
+*/
+
+// app.use(cors(corsOptions)); // Se puede añadir a todas las ruta
+
 app.get('/', (req, res) => {
   res.send('Bienvenido al despliegue del servidor de Lucas!');
-});
+})
 
-app.get('/messages', (req, res) => {
-  // Devolver mensajes alamacenados en la BBDD
-  const apikey = req.headers['apikey'];
-if (apikey === APIKEY) {
-    res.json(getMessages());
-    return res.status(200).send('OK');
-  }
-});
+app.get('/messages', (req, res) => {    
+   // Devolver mensajes alamacenados en la BBDD
+   const apikey = req.headers['apikey'];
+   if (apikey !== APIKEY){
+     res.json(getMessages());
+     return res.status(200).send('OK');  
 
-app.post('/messages', (req, res) => {
-  // Guardar mensajes en la BBDD
-  const apikey = req.headers['apikey'];
+    }else if (apikey === APIKEY){
+      res.json(getMessages());
+      return res.status(200).send('OK');  
+   }
+})
+
+app.post('/messages', (req, res) => {    
+   // Guardar mensajes en la BBDD
+   const apikey = req.headers['apikey'];
   if (apikey !== APIKEY) {
     return res.status(401).send('Unauthorized');
   }
 
   // Manda el mensaje por la query y lo añadade a la BBDD
-  const message = req.body.message;
+  const message = req.body.message;  
   if (message) {
     addMessage(message);
     res.status(201).send('Message added');
   } else {
     res.status(400).send('Bad Request');
   }
-});
+})
 
-if (process.env.NODE_ENV === 'production') {
+addMessage("Este es un mensaje hardcodeado");
+console.log(process.env.NODE_ENV); // Process.env busca la variable de entorno NODE_ENV en el proyecto
+if(process.env.NODE_ENV === 'production'){
+
   const options = {
     key: fs.readFileSync(path.join(__dirname, 'privkey.pem')),
     cert: fs.readFileSync(path.join(__dirname, 'fullchain.pem'))
   };
 
-  //create an https server
-  https.createServer(options, app).listen(3000, () => {
-    console.log('Servidor corriendo en https://dev5.cyberbunny.online:3000');
+  // Crear el servidor HTTPS
+  https.createServer(options, app).listen(port, () => {
+    console.log(`Server started on https://dev5.cyberbunny.online:${port}`);
   });
-} else {
-  http.createServer(app).listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+  
+}else{
+
+  // Crear el servidor HTTP
+  http.createServer(app).listen(port, () => {
+    console.log(`Server started on http://localhost:${port} o http://yourIP:${port}`);
   });
 }
